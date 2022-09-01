d12717bbbdde2ae54d0301cc48198d25e962159c
Carlos Wagner <wagner30023@gmail.com>
2022-09-01 06:05:57 -0300

    usando hooks para concluir a todo list

--- src/App.js
+++ src/App.js
@@ -11,6 +11,24 @@ function App() {
   const [todos,setTodos]= useState([]);
   const [Loading,setLoading] = useState(false);
 
+  // Load todos on page load
+  useEffect(() => {
+    const loadData = async() => {
+      setLoading(true);
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((error) => console.log(error));
+
+        setLoading(false);
+
+        setTodos(res);
+    };
+
+    loadData();
+  },[]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(title);
@@ -31,11 +49,43 @@ function App() {
     });
 
     // Envio para api
-    console.log(todo);
-
+    // console.log(todo);
+    //  utiliza o spread operator ppara pegar todos os todos e iniserir em um novo todo
+    setTodos((prevState) => [...prevState,todo]);
     setTitle("");
     setTime("");
   };
+
+
+
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: 'DELETE',
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  }
+
+  const handleEdit = async (todo) => {
+
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: 'PUT',
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    });
+
+    setTodos((prevState) => 
+      prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
+  }
+
+  if(Loading){
+    return <p> Carregando...</p>
+  }
 
   return (
     <div className="App">
@@ -78,6 +128,18 @@ function App() {
       <div class="list-todo" >
         <h2> Lista de tarefas: </h2>
         {todos.length === 0 && <p> Não há tarefas! </p> }
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}> {todo.title} </h3>
+            <p>Duração: {todo.time}</p>
+            <div className="actions">
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
